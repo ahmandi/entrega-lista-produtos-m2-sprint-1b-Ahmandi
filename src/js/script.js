d41ffea616dc3239fda44b19b00dc81d5e8b7bc0
Amanda <amandard94@gmail.com>
2022-06-02 15:11:06 -0300

    tarefa

--- src/js/script.js
+++ src/js/script.js
@@ -14,32 +14,106 @@ botaoMostrarTodos.addEventListener('click', filtrarPorTodos);
 
 let busca = document.querySelector('.campoBuscaPorNome')
 
+let listaProdutosCarrinho = [];
 
 function montarListaProdutos(listaProdutos) {
     ul.innerHTML = '';
 
     listaProdutos.forEach((produto) => {
-        const li = document.createElement('li');
-        const img = document.createElement('img');
-        const h3 = document.createElement('h3');
-        const p = document.createElement('p');
-        const span = document.createElement('span');
+        const li     = document.createElement('li');
+        const img    = document.createElement('img');
+        const h3     = document.createElement('h3');
+        const p      = document.createElement('p');
+        const ol     = document.createElement('ol');
+        const span   = document.createElement('span');
+        const div    = document.createElement('div');
+        const button = document.createElement('button');
+        button.classList.add('estiloGeralBotoes', 'estiloGeralBotoes--comprar')
+        button.addEventListener('click', addItem)
 
-        img.src = produto.img;
-        img.alt = produto.nome;
-        h3.innerText = produto.nome;
-        p.innerText = produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        span.innerText = produto.secao;
+        button.innerText= 'Comprar'
+        button.id       = produto.id;
+        img.src         = produto.img;
+        img.alt         = produto.nome;
+        h3.innerText    = produto.nome;
+        span.innerText  = produto.secao;
+        p.innerText     = produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        produto.componentes.forEach((componente) => {
+            const li     = document.createElement('li');
+            li.innerHTML = componente
+            ol.appendChild(li)
+        })
 
-        li.append(img, h3, p, span);
+        div.append(p, button)
+        li.append(img, h3, span, ol, div);
 
         ul.appendChild(li);
     });
 }
 
+function criarProdutosCart() {
+    let limpar = document.querySelector('.cart__checkout--product')
+    limpar.innerHTML = '';
+
+    for(let i = 0; i < listaProdutosCarrinho.length; i++){
+        let cartProduct         = document.querySelector('.cart__checkout--product')
+        let productPositioning  = document.createElement('div')
+        let productInfoContainer= document.createElement('div')
+        let productContainer    = document.createElement('div')
+        let productImg          = document.createElement('img')
+        let productName         = document.createElement('h3')
+        let productType         = document.createElement('p')
+        let productPrice        = document.createElement('span')
+        let productRemoval      = document.createElement('button')
+        
+        productInfoContainer.classList.add('cart_checkout--product--info')
+        productContainer.classList.add('product__container')
+        productPositioning.classList.add('product-positioning')
+        productImg.classList.add('product__img')
+        productName.classList.add('product__name')
+        productType.classList.add('product__type')
+        productPrice.classList.add('product__price')
+        productRemoval.classList.add('trash')
+        productPositioning.id = 'removal';
+
+        productRemoval.addEventListener('click', removeItem)
+
+        productImg.src          = listaProdutosCarrinho[i].img
+        productName.innerText   = listaProdutosCarrinho[i].nome
+        productType.innerText   = listaProdutosCarrinho[i].secao
+        productPrice.innerText  = listaProdutosCarrinho[i].preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        productRemoval.id       = listaProdutosCarrinho[i].id
+        productRemoval.innerHTML= `<i class="fa-solid fa-trash trash"></i>`
+
+        productInfoContainer.append(productName, productType, productPrice);
+        productContainer.append(productImg, productInfoContainer);
+        productPositioning.append(productContainer, productRemoval)
+        cartProduct.append(productPositioning);
+    }
+}
+
+function addItem(event){
+    let productId  = event.target.id
+    let productReceiver = produtos.find(item => item.id == productId)
+    
+    listaProdutosCarrinho.push(productReceiver)
+    totalUpdate(listaProdutosCarrinho)
+    criarProdutosCart()
+}
+
+function removeItem(event){
+    let removeBtn   = event.target;
+    const productId = removeBtn.id
+    removeBtn.closest('.product-positioning').remove()
+
+    const indice = listaProdutosCarrinho.findIndex(produto => produto.id == productId)
+    listaProdutosCarrinho.splice(indice, 1)
+    totalUpdate(listaProdutosCarrinho)
+    criarProdutosCart()
+}
+
 function filtrarPorTodos() {
     montarListaProdutos(produtos);
-    totalUpdate(produtos)
 }
 
 function filtrarPorHortifruti() {
@@ -47,7 +121,6 @@ function filtrarPorHortifruti() {
         return produto.secao === 'Hortifruti';
     });
     montarListaProdutos(listaHortifruti);
-    totalUpdate(listaHortifruti)
 }
 
 function filtrarPorPanificadora() {
@@ -55,28 +128,30 @@ function filtrarPorPanificadora() {
         return produto.secao === 'Panificadora';
     });
     montarListaProdutos(listaPanificadora);
-    totalUpdate(listaPanificadora)
 }
 
 function filtrarPorLaticínio() {
     const listaLaticínio = produtos.filter((produto) => {
-        return produto.secao === 'Laticínio';
+        return produto.secao === 'Laticinio';
     });
     montarListaProdutos(listaLaticínio);
-
-    totalUpdate(listaLaticínio)
 }
 
-function totalUpdate(produtos){
-    let preco = document.getElementById('precoTotal')
+function totalUpdate(listaProdutosCarrinho){
+    const itemQty   = document.querySelector('.quantity--value')
+    const itemTotal = document.querySelector('.totalPrice--value')
 
-    let precoTotal = 0
+    let quantidade  = listaProdutosCarrinho.length
+    let precoTotal  = 0
 
-    produtos.forEach(item => {
+    listaProdutosCarrinho.forEach(item => {
         precoTotal += Number(item.preco)
     })
+    console.log(listaProdutosCarrinho)
 
-    preco.innerText = `${precoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`
+    itemTotal.innerText = `${precoTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`
+
+    itemQty.innerText   = quantidade
 }
 
 function searchBox(){
@@ -98,5 +173,4 @@ function filteredSearch(input, produtos){
         }
     })
     montarListaProdutos(arr)
-    totalUpdate(arr)
 }
